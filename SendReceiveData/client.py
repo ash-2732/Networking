a5a3efa56defcba4e161525246cfb9939a84b952
Abdullah Ashik <121299454+ash-2732@users.noreply.github.com>
2024-01-26 21:31:54 +0600

    second one

--- SendReceiveData/client.py
+++ SendReceiveData/client.py
@@ -5,3 +5,4 @@ s.connect((socket.gethostname(), 1234))  # connect to the server
 
 msg = s.recv(1024) # 1024 is the buffer size
 print(msg.decode("utf-8")) # decode the message from the server
+print(msg.decode("utf-8")) # decode the message from the server
